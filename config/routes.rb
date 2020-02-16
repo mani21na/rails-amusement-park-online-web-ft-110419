bdee8ec2a7a524d68c338473cb858ee0cbee0744
Hanna Lee <mani21na@gmail.com>
2020-02-16 04:10:32 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # For deta  root 'users#welcome'
+  get '/signin' => 'users#signin', as: :signin
+  post '/login' => 'users#login', as: :login
+  get '/logout' => 'users#logout', as: :logout
+  resources :users, only: [:new, :show, :create, :edit, :update]
+  resources :attractions
+  post '/attractions/ride' => 'attractions#ride'ils on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
