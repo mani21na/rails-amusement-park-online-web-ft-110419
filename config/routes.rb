16fb9a6d7fd8adc77a303de95e134ad6868c02d7
Hanna Lee <mani21na@gmail.com>
2020-02-16 04:47:33 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
-  # For deta  root 'users#welcome'
+
+  root 'users#welcome'
   get '/signin' => 'users#signin', as: :signin
   post '/login' => 'users#login', as: :login
   get '/logout' => 'users#logout', as: :logout
   resources :users, only: [:new, :show, :create, :edit, :update]
   resources :attractions
-  post '/attractions/ride' => 'attractions#ride'ils on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  post '/attractions/ride' => 'attractions#ride'
+
 end
